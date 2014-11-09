3989d313c341e7cdeb95ddc27ad0816430471175
tanem <tane.morgan@gmail.com>
2014-11-10 10:29:45 +1300

    Add AMD module

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   entry: './lib/app/index.js',
   output: {
     filename: './build/bundle.js'
+  },
+  resolve: {
+    alias: {
+      jquery: path.join(__dirname, 'vendor/jquery-2.1.1.js')
+    }
   }
 };
