0f38ed0961371e9e0efabc87a948e7e9f0333476
tanem <tane.morgan@gmail.com>
2014-11-09 15:17:16 +1300

    Clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,5 @@ module.exports = {
   entry: './lib/app/index.js',
   output: {
     filename: './build/bundle.js'
-  }//,
-  // plugins: [
-  //   new webpack.optimize.UglifyJsPlugin()
-  // ],
-  // devtool: '#source-map'
+  }
 };
